1663742634c9e5e60459cff3fbe0568f73ca9961
koniman <user@USER-no-MacBook-Pro.local>
2020-05-27 12:05:44 +0900

    seedデータの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
 EMAIL = 'test@example.com'
 PASSWORD = 'password'
+ADMIN_EMAIL = 'admin@example.com'
+ADMIN_PASSWORD = 'password'
 
 # テストユーザーが存在しないときだけ作成
 User.find_or_create_by!(email: EMAIL) do |user|
   user.password = PASSWORD
   puts 'ユーザーの初期データインポートに成功しました。'
 end
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+#AdminUser.create!(email: 'admin@example.com', password: 'password', #password_confirmation: 'password') if Rails.env.development?
+AdminUser.find_or_create_by!(email: ADMIN_EMAIL) do |adminuser|
+  adminuser.password = ADMIN_PASSWORD
+  puts '管理者の初期データインポートに成功しました。'
+end
