c8fd7d0c62f035a1aa1af3aa32e0ad16e4179aaf
Nao <dece.0.na.12@gmail.com>
2020-05-28 14:03:31 +0900

    修正20_Rails動画教材ページネーションの実装

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
     def index
         @movies = Movie.all
-        @movies = Movie.all.page(params[:page]).per(4)
+        @movies = Movie.all.page(params[:page]).per(10)
     end
 end
