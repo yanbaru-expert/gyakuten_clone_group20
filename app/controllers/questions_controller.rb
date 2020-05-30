0af75071f067afe0b95d9b9b5ecc28efa59b0697
masa <mk1988senna.startline@gmail.com>
2020-05-30 13:32:56 +0900

    質問の新規投稿機能を追加

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,16 @@
 class QuestionsController < ApplicationController
   def index
     @questions = Question.all.order(id: "DESC")
+    @question = Question.new
   end
+
+  def create
+    @question = Question.create(question_params)
+  end
+
+  private
+
+    def question_params
+      params.require(:question).permit(:title, :detail)
+    end
 end
