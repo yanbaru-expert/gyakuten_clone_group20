2643b68f2f46134954d9b748aec62c58177f3fc1
masa <mk1988senna.startline@gmail.com>
2020-06-02 12:48:58 +0900

    マイグレーションしました

--- db/schema.rb
+++ db/schema.rb
@@ -67,6 +67,8 @@ ActiveRecord::Schema.define(version: 2020_05_31_115824) do
     t.text "detail"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "question_id"
+    t.index ["question_id"], name: "index_solutions_on_question_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -81,4 +83,5 @@ ActiveRecord::Schema.define(version: 2020_05_31_115824) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "solutions", "questions"
 end
