ab46fb5539cb87d27b2931e3cbdec5a0ddbbc3f9
naohirokooi <63993885+naohirokooi@users.noreply.github.com>
2020-06-21 17:20:29 +0900

    Feature/texts (#43)
    text教材の一覧ページを実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_15_132632) do
+ActiveRecord::Schema.define(version: 2020_06_19_092629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -116,6 +116,14 @@ ActiveRecord::Schema.define(version: 2020_06_15_132632) do
   create_table "talks", force: :cascade do |t|
     t.string "title"
     t.string "url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "texts", force: :cascade do |t|
+    t.string "genre"
+    t.string "title"
+    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
