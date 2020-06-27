dcd55cb92bfd1b5613bef5c87bad6aeae7d9f28a
masa <mk1988senna.startline@gmail.com>
2020-06-27 23:04:05 +0900

    systemで記述

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,91 +14,15 @@ AdminUser.find_or_create_by!(email: ADMIN_EMAIL) do |adminuser|
   puts '管理者の初期データインポートに成功しました。'
 end
 
-
-require "csv"
-
-CSV.foreach('db/csv_data/aws_text_data.csv', headers: true) do |row|
-  AwsText.create(
-    title: row['title'],
-    content: row['content']
-  )
-end
-
-CSV.foreach('db/csv_data/edit_movie_data.csv', headers: true) do |row|
-  EditMovie.create(
-    title: row['title'],
-    url: row['url']
-  )
-end
-
-CSV.foreach('db/csv_data/line_data.csv', headers: true) do |row|
-  Line.create(
-    genre: row['genre'],
-    title: row['title'],
-    contents: row['contents']
-  )
-end
-
-CSV.foreach('db/csv_data/live_cording_data.csv', headers: true) do |row|
-  LiveCording.create(
-    title: row['title'],
-    url: row['url']
-  )
-end
-
-CSV.foreach('db/csv_data/marketing_movie_data.csv', headers: true) do |row|
-  Marketing.create(
-    title: row['title'],
-    url: row['url']
-  )
-end
-
-CSV.foreach('db/csv_data/money_movie_data.csv', headers: true) do |row|
-  Money.create(
-    title: row['title'],
-    url: row['url']
-  )
-end
-
-CSV.foreach('db/csv_data/movie_data.csv', headers: true) do |row|
-  Movie.create(
-    title: row['title'],
-    url: row['url']
-  )
-end
-
-CSV.foreach('db/csv_data/php_movie_data.csv', headers: true) do |row|
-  Php.create(
-    title: row['title'],
-    url: row['url']
-  )
-end
-
-CSV.foreach('db/csv_data/question_data.csv', headers: true) do |row|
-  Question.create(
-    title: row['title'],
-    detail: row['detail']
-  )
-end
-
-CSV.foreach('db/csv_data/talks_movie_data.csv', headers: true) do |row|
-  Talk.create(
-    title: row['title'],
-    url: row['url']
-  )
-end
-
-CSV.foreach('db/csv_data/text_data.csv', headers: true) do |row|
-  Text.create(
-    genre: row['genre'],
-    title: row['title'],
-    content: row['content']
-  )
-end
-
-CSV.foreach('db/csv_data/writing_deta.csv', headers: true) do |row|
-  Writing.create(
-    title: row['title'],
-    url: row['url']
-  )
-end
+system("rake import_csv:aws_texts")
+system("rake import_csv:edit_movie_data")
+system("rake import_csv:line_data")
+system("rake import_csv:live_cording_data")
+system("rake import_csv:marketings_data")
+system("rake import_csv:money_data")
+system("rake import_csv:movie_data")
+system("rake import_csv:php_data")
+system("rake import_csv:questions")
+system("rake import_csv:talks_data")
+system("rake import_csv:text_data")
+system("rake import_csv:writing_data")
