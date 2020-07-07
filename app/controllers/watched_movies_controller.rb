df0c064b896ab1793b4da3b61e1f5409e2a1c540
Seidai Inoue <shengdajingshang@gmail.com>
2020-07-07 22:07:16 +0900

    非同期が実行されない

--- app/controllers/watched_movies_controller.rb
+++ app/controllers/watched_movies_controller.rb
@@ -1,9 +1,11 @@
 class WatchedMoviesController < ApplicationController
   def create
-      current_user.watched_movies.create!(movie_id: params[:movie_id])
-    end
+      @movie = current_user.watched_movies.create!(movie_id: params[:movie_id])
+      @movie.save
+  end
 
   def destroy
-      current_user.watched_movies.find_by(movie_id: params[:movie_id]).destroy!
+      @movie = current_user.watched_movies.find_by(movie_id: params[:movie_id])
+      @movie.destroy!
   end
 end
