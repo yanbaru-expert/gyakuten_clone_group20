78b85162f418806344afe3af2374a269c7048ffc
masa <mk1988senna.startline@gmail.com>
2020-07-13 20:24:12 +0900

    絞込み検索機能

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,17 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("bootstrap/dist/js/bootstrap")
+
+$(function () {
+    searchText = $(this).val();
+    $('.card-title').each(function() {
+        if($(this).text.indexOf(searchText) != -1){
+            $(this).removeClass("d-none")
+            $(this).addClass("d-flex")
+        }
+        else{
+            $(this).removeClass("d-flex")
+            $(this).addClass("d-none")
+        }
+    });
+});
